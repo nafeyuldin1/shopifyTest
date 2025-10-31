9c020d48791b37cd6c87aba0175b3e09808c8204
nafeyuldin <nafeyuldin.official.rs@gmail.com>
2025-11-01 03:53:49 +0500

    fiels udpates

---
+++ assets/custom.js
@@ -0,0 +1,22 @@
+/*
+* Custom Store Theme
+*
+* Use this file to add custom Javascript to your theme. Keeping your custom
+* Javascript in this file will make it easier to update the theme. In order
+* to use this file you will need to open layout/theme.liquid and uncomment
+* the custom.js script import line near the bottom of the file.
+*/
+
+
+(function() {
+  // Add custom code below this line
+
+
+  
+
+
+
+
+  // ^^ Keep your scripts inside this IIFE function call to 
+  // avoid leaking your variables into the global scope.
+})();
